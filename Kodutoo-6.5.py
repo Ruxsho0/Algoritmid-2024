c87b17b88fc0586c781677fbb0d9d5ff122ac745
Ruxsho0 <159537200+Ruxsho0@users.noreply.github.com>
2024-12-21 16:09:46 +0200

    exc 5 done

---
+++ Kodutoo-6.5.py
@@ -0,0 +1,8 @@
+# Ülesanne 5: Graafide Värvimise Probleem
+
+# Graafide värvimise probleem (Graph Coloring Problem) on ülesanne, kus tuleb määrata graafi tippudele värvid nii, et üksteisega ühendatud tipud ei oleks sama värvi. 
+# Probleemi eesmärk on kasutada võimalikult vähe erinevaid värve.
+
+# Graafide värvimise probleem on oluline, kuna see on seotud paljude praktiliste ülesannetega, nagu näiteks:
+# 1. Ressursside jaotamine: Näiteks eksamite ajakava koostamine, kus eksamid, millel on ühised õpilased, ei tohi toimuda samal ajal.
+# 2. Konfliktide lahendamine: Näiteks raadiosageduste jaotamine, kus lähedal asuvad saatjad ei tohi kasutada sama sagedust, et vältida häireid.

27419818aa33ca1be684dcc653db8dcbac4c6ef7
Ruxsho0 <159537200+Ruxsho0@users.noreply.github.com>
2024-12-21 16:20:30 +0200

    bonus exc done

---
+++ Kodutoo-6.6-boonus.py
@@ -0,0 +1,34 @@
+# Boonus
+
+# P vs NP Probleemi Ülevaade:
+
+# P vs NP probleem on üks olulisemaid ja tuntumaid lahendamata probleeme arvutiteaduses. Probleem keskendub küsimusele,
+# kas iga probleem, mille lahendust saab kontrollida polünoomajas (NP),
+# saab ka lahendada polünoomajas (P). Teisisõnu, kas P = NP?
+
+# P (Polynomial time) klass sisaldab probleeme, mida saab lahendada deterministliku Turingi masina abil polünoomajas.
+# NP (Nondeterministic Polynomial time) klass sisaldab probleeme, mille lahendusi saab kontrollida polünoomajas,
+# kuid ei pruugi olla teada, kas neid saab ka polünoomajas lahendada.
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
+# P vs NP Probleemi Tähtsus:
+# P vs NP probleem on fundamentaalne, kuna see puudutab arvutiteaduse ja algoritmide teooria aluseid. Kui P = NP, tähendaks see,
+# et kõik probleemid, mille lahendusi saab kontrollida polünoomajas, saab ka lahendada polünoomajas.
+# See avaks uued võimalused paljudes valdkondades, sealhulgas krüptograafias, optimeerimises, tehisintellektis ja paljudes teistes.
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
+# P vs NP Probleemi Lahendamise Olulisus ja Tagajärjed:
+# Kui suudaksime tõestada, et P = NP, oleks sellel tohutu mõju arvutiteadusele ja paljudele praktilistele rakendustele.
+
+# Näiteks krüptograafia, mis tugineb teatud probleemide lahendamise keerukusele, muutuks ebaturvaliseks,
+# kuna paljusid praegu turvalisi krüptosüsteeme saaks murda polünoomajas.
+
+# Teisest küljest, kui suudaksime tõestada, et P ≠ NP, kinnitaks see, et teatud probleemid on lahendamatult keerulised ja nende lahendamine nõuab eksponentsiaalset aega.
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
+# Kokkuvõttes on P vs NP probleem keskne küsimus arvutiteaduses, mille lahendamine aitaks paremini mõista arvutuslikku keerukust ja algoritmide piire.
+
+# Polünoom sünonüüm on hulkliige

819346145fe6777be771cf29423e7166d0c3c34f
Ruxsho0 <159537200+Ruxsho0@users.noreply.github.com>
2024-12-21 14:25:15 +0200

    exc 1 done

---
+++ Kodutoo-6.1.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        vertex = queue.popleft()
+        print(vertex, end=" ")
+
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Näide graafist, mis on esitatud naabrusloendina
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+# Teosta BFS alustades tipust 'A'
+bfs(graph, 'A')
+
